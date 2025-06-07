f03ba4657da3bade4964388dbb41cfe68d083fca
echo <callum@conicu.com.au>
2025-06-07 17:51:13 +1000

    Update generate.keypair.py

--- mesh/scripts/generate.keypair.py
+++ mesh/scripts/generate.keypair.py
@@ -15,7 +15,7 @@ private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048)
 public_key = private_key.public_key()
 
 # Write private key
-with open(f"../keys/public/{key_name}_private.pem", "wb") as f:
+with open(f"keys/public/{key_name}_private.pem", "wb") as f:
     f.write(private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
@@ -23,7 +23,7 @@ with open(f"../keys/public/{key_name}_private.pem", "wb") as f:
     ))
 
 # Write public key
-with open(f"../keys/private/{key_name}_public.pem", "wb") as f:
+with open(f"keys/private/{key_name}_public.pem", "wb") as f:
     f.write(public_key.public_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PublicFormat.SubjectPublicKeyInfo
